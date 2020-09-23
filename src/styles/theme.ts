18efa256c10062f066f0c24eb36cb84cd984548c
andersonsilva019 <andeerson0017@gmail.com>
2020-09-23 18:32:14 -0300

    :recycle: fix: adjust theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,13 +9,13 @@ export default {
     small: '1.2rem',
     default: '1.4rem',
     large: '1.6rem',
-    tiny: '1rem',
+    tiny: '7.0rem',
   },
   transition: {
     default: '200ms ease-in-out',
   },
   shadows: {
-    default: '0 0.5rem 2rem rgb(76, 113, 251,0.3)',
+    default: '0 0.5rem 2rem rgb(76, 113, 251,0.8)',
     flat: '0 0.2rem 0.2rem rgba(0, 0, 0, 0.08)',
   },
 } as const;
