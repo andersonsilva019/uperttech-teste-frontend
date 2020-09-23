1861ad190b85d39972a924fa30870437f6a0919f
andersonsilva019 <andeerson0017@gmail.com>
2020-09-23 14:58:50 -0300

    initial struct

---
+++ src/assets/images/index.ts
@@ -0,0 +1,2 @@
+export { ReactComponent as ArtImage} from './art1.svg';
+export { ReactComponent as BackgroundImage} from './background1.svg';
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <div>Main</div>
+    </Container>
+  );
+};
+
+export default Main;
